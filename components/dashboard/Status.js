6d69cec92df52acd70e382f2a4cbc7dba35b27fe
lurns <lrnlee08@gmail.com>
2022-03-06 22:22:39 -0600

    display message logic

--- components/dashboard/Status.js
+++ components/dashboard/Status.js
@@ -1,13 +1,42 @@
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useAuth } from "../../store/auth-context";
 import UserPic from "../ui/UserPic";
 import MessageBubble from "../ui/MessageBubble";
 import UploadNewPic from "../ui/UploadNewPic";
+import { WorkoutContext } from "../../store/workout-context";
+import { DateTime } from "luxon";
+import { server } from "../../lib/config";
+
+const timePassed = (lastWorkout) => {
+	const currentDate = DateTime.fromJSDate(new Date());
+	const lastWorkoutDate = DateTime.fromISO(lastWorkout.date);
+	const timePassed = currentDate.diff(lastWorkoutDate, ["hours"])
+	return timePassed.values.hours;
+}
+
+const fetchMessage = async (messageType) => {
+	try {
+		const msg = { message_type: messageType }
+		const response = await fetch(`${server}/api/message`, {
+			method: 'POST',
+			body: JSON.stringify(msg),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		return response.json();
+
+	} catch (e) {
+		console.log('error fetching message');
+		console.log('error ', e);
+	}
+}
 
 const Status = () => {
 	const { auth, user, setUser } = useAuth();
-	
-	// TODO: logic for determining what message to deliver
+	const { workouts } = useContext(WorkoutContext);
+	const [message, setMessage] = useState('');
 
 	useEffect(() => {
 		if (localStorage.getItem('rally_storage') !== '') {
@@ -15,12 +44,41 @@ const Status = () => {
 		}
 	}, [auth]);
 
+	useEffect(() => {
+		if (workouts[0]) {
+			const hours = timePassed(workouts[0].workout);
+			let messageType = '';
+
+			// determine message
+			if (hours < 48) {
+				// on track
+				messageType = 'ON_TRACK';
+			} else if (hours > 48 && hours < 72) {
+				// motivational
+				messageType = 'MOTIVATIONAL';
+			} else if (hours > 72 && hours < 96) {
+				// do better
+				messageType = 'DO_BETTER';
+			} else {
+				// do better-er
+				messageType = 'DO_BETTER_ER';
+			}
+
+			// get message, set it
+			fetchMessage(messageType)
+				.then(res => {
+					setMessage(res)
+			});
+		}
+
+	}, [workouts])
+
 	return (
 		<div>
 			<h3 className="font-black text-4xl text-yellow-500 bg-yellow-900 w-fit p-2">
 				Hey, { user?.user?.nickname }!
 			</h3>
-			<MessageBubble message="Test message in heah" />
+			<MessageBubble message={message?.message ? message.message : ''} />
 			<UserPic />
 			<UploadNewPic />
 		</div>
---
+++ pages/api/message.js
@@ -0,0 +1,58 @@
+import { MongoClient, ObjectId } from 'mongodb';
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { ironOptions } from '../../lib/config';
+
+export default withIronSessionApiRoute(handler, ironOptions);
+
+async function handler (req, res) {
+    if (req.method === 'POST' && req.body.message_type && req.session.user.id) {
+        try {
+			const messageType = req.body.message_type;
+
+            // add to db
+            const client = await MongoClient.connect(process.env.MONGO_CONNECT);
+            const db = client.db();
+
+            const messagesCollection = db.collection('messages');
+
+			const cursor = await messagesCollection
+				.find({ 
+					"message.user_id": req.session.user.id, 
+					"message.message_type": messageType,
+				});
+
+			const messages = await cursor.toArray();
+			//console.log(messages);
+
+			client.close();
+
+			// get a random message / send default
+			if (messages.length === 0) {
+				if (messageType === 'ON_TRACK') {
+					await res.status(200).json({ message: `I'm doing great!` });
+				} else if (messageType === 'MOTIVATIONAL') {
+					await res.status(200).json({ message: `It's about time to get back out there` });
+				} else if (messageType === 'DO_BETTER') {
+					await res.status(200).json({ message: `Liiiiiiike any day now` });
+				} else {
+					await res.status(200).json({ message: `Never give up, never surrender!` });
+				}
+			}
+
+			// only one entry
+			if (messages.length === 1) {
+				await res.status(201).json({ message: messages[0].message.message_body });
+			}
+
+			// choose random
+			if (messages.length > 1) {
+				await res.status(201).json(messages);
+			}
+
+            
+        } catch (e) {
+            console.log('error ', e);
+			await res.status(403).json({error: 'unable to get message'});
+        }
+    } 
+}
