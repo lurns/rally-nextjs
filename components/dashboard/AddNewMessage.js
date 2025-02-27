6efccab62357c627e856b69a64028411229b1640
Lauren Lee <lrnlee08@gmail.com>
2025-02-27 16:05:30 -0600

    formatting

--- components/dashboard/AddNewMessage.js
+++ components/dashboard/AddNewMessage.js
@@ -1,103 +1,108 @@
 import { useRef, useState } from "react";
 import { useAuth } from "../../store/auth-context";
-import ErrorMessage from '../../components/ui/ErrorMessage';
+import ErrorMessage from "../../components/ui/ErrorMessage";
 import SuccessMessage from "../ui/SuccessMessage";
 import { useRouter } from "next/router";
-import { DO_BETTER, DO_BETTER_ER, MessageType, MOTIVATIONAL, ON_TRACK } from "../../constants/messageType";
+import {
+  DO_BETTER,
+  DO_BETTER_ER,
+  MessageType,
+  MOTIVATIONAL,
+  ON_TRACK,
+} from "../../constants/messageType";
 
 const AddNewMessage = (props) => {
-	const [error, setError] = useState(false);
-	const [success, setSuccess] = useState(false);
-	const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-	const { auth, user, setUser } = useAuth();
-	const router = useRouter();
-    const messageBodyRef = useRef();
-    const messageTypeRef = useRef();
+  const { auth, user, setUser } = useAuth();
+  const router = useRouter();
+  const messageBodyRef = useRef();
+  const messageTypeRef = useRef();
 
-    const submitMessageHandler = async (event) => {
-        event.preventDefault();
-		setLoading(true);
+  const submitMessageHandler = async (event) => {
+    event.preventDefault();
+    setLoading(true);
 
-		// TODO: error handling
+    // TODO: error handling
 
-        const messageData = {
-            message_body: messageBodyRef.current.value,
-            message_type: messageTypeRef.current.value,
-			user_id: user._id,
-        }
+    const messageData = {
+      message_body: messageBodyRef.current.value,
+      message_type: messageTypeRef.current.value,
+      user_id: user._id,
+    };
 
-		const response = await fetch('/api/new-message', {
-			method: 'POST',
-			body: JSON.stringify(messageData),
-			headers: {
-				'Content-Type': 'application/json'
-			}
-			});
-	
-			const data = await response.json();
-			console.log(data);
-	
-			if (!data.error) {
-				// clear fields, give success msg
-				setLoading(false);
-				document.getElementById('messageBody').value = '';
-				setSuccess(true);
-				props.setMessages((current) => [data, ...current]);
-				props.closeModal ? props.closeModal() : router.push('/dash');
-			} else {
-				setLoading(false);
-				setError(true);
-			}
+    const response = await fetch("/api/new-message", {
+      method: "POST",
+      body: JSON.stringify(messageData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    if (!data.error) {
+      // clear fields, give success msg
+      setLoading(false);
+      document.getElementById("messageBody").value = "";
+      setSuccess(true);
+      props.setMessages((current) => [data, ...current]);
+      props.closeModal ? props.closeModal() : router.push("/dash");
+    } else {
+      setLoading(false);
+      setError(true);
     }
+  };
 
-  	return (
-		<div>
-			<h3 className="font-black text-3xl text-sky-900">
-				Add New Message
-			</h3>
-			{error && !loading ? <ErrorMessage message="Error adding message" /> : ''}
-			{success && !loading ? <SuccessMessage message="Message added!" /> : ''}
-			<form id="addNewMessageForm" className="mt-5" onSubmit={submitMessageHandler}>
-			<div className="flex flex-col mb-3">
-				<label 
-					htmlFor="messageType"
-					className="text-left text-slate-500"
-				>Message Type
-				</label>
-				<select 
-					name="messageType"
-					id="messageType"
-					className="form-select border-1 border-slate-300 bg-slate-100 rounded-lg p-2"
-					ref={messageTypeRef}
-					required
-				>
-					<option value={ON_TRACK}>{MessageType.ON_TRACK}</option>
-					<option value={MOTIVATIONAL}>{MessageType.MOTIVATIONAL}</option>
-					<option value={DO_BETTER}>{MessageType.DO_BETTER}</option>
-					<option value={DO_BETTER_ER}>{MessageType.DO_BETTER_ER}</option>
-				</select>
-			</div>
-			<div className="flex flex-col mb-3">
-				<label 
-					htmlFor="messageBody"
-					className="text-left text-slate-500"
-				>Message Body
-				</label>
-				<textarea
-					name="messageBody"
-					id="messageBody"
-					className="form-textarea border-1 border-slate-300 bg-slate-100 rounded-lg p-2 resize-none"
-					ref={messageBodyRef}
-					placeholder="Be gentle..."
-					maxLength="300"
-					required
-				/>
-			</div>
-			<div className="flex flex-col mb-3">
-				<button id="submitMessageButton"
-					type="submit" 
-					className="
+  return (
+    <div>
+      <h3 className="font-black text-3xl text-sky-900">Add New Message</h3>
+      {error && !loading ? <ErrorMessage message="Error adding message" /> : ""}
+      {success && !loading ? <SuccessMessage message="Message added!" /> : ""}
+      <form
+        id="addNewMessageForm"
+        className="mt-5"
+        onSubmit={submitMessageHandler}
+      >
+        <div className="flex flex-col mb-3">
+          <label htmlFor="messageType" className="text-left text-slate-500">
+            Message Type
+          </label>
+          <select
+            name="messageType"
+            id="messageType"
+            className="form-select border-1 border-slate-300 bg-slate-100 rounded-lg p-2"
+            ref={messageTypeRef}
+            required
+          >
+            <option value={ON_TRACK}>{MessageType.ON_TRACK}</option>
+            <option value={MOTIVATIONAL}>{MessageType.MOTIVATIONAL}</option>
+            <option value={DO_BETTER}>{MessageType.DO_BETTER}</option>
+            <option value={DO_BETTER_ER}>{MessageType.DO_BETTER_ER}</option>
+          </select>
+        </div>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="messageBody" className="text-left text-slate-500">
+            Message Body
+          </label>
+          <textarea
+            name="messageBody"
+            id="messageBody"
+            className="form-textarea border-1 border-slate-300 bg-slate-100 rounded-lg p-2 resize-none"
+            ref={messageBodyRef}
+            placeholder="Be gentle..."
+            maxLength="300"
+            required
+          />
+        </div>
+        <div className="flex flex-col mb-3">
+          <button
+            id="submitMessageButton"
+            type="submit"
+            className="
 						min-w-full 
 						mt-5 
 						p-4 
@@ -112,15 +117,14 @@ const AddNewMessage = (props) => {
 						shadow-lg
 						disabled:bg-slate-400
 					"
-					disabled={loading ? true : false}
-				>
-					Add New Message
-				</button>
-			</div>
-
-			</form>
-	</div>
-  )
-}
+            disabled={loading ? true : false}
+          >
+            Add New Message
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddNewMessage;
--- pages/api/new-message.js
+++ pages/api/new-message.js
@@ -1,36 +1,35 @@
-import { MongoClient } from 'mongodb';
-import { getIronSession } from 'iron-session'
-import { ironOptions } from '../../lib/config';
+import { MongoClient } from "mongodb";
+import { getIronSession } from "iron-session";
+import { ironOptions } from "../../lib/config";
 
-export default async function handler (req, res) {
-	const session = await getIronSession(req, res, ironOptions)
-    if (req.method === 'POST' && req.body.message_type && req.body.message_body) {
-        try {
-			const date = new Date();
-			
-			const message = {
-				message_type: req.body.message_type,
-				message_body: req.body.message_body,
-				user_id: req.body.user_id, // or req.session
-				date: date,
-			}
+export default async function handler(req, res) {
+  const session = await getIronSession(req, res, ironOptions);
+  if (req.method === "POST" && req.body.message_type && req.body.message_body) {
+    try {
+      const date = new Date();
 
-            // add to db
-            const client = await MongoClient.connect(process.env.MONGO_CONNECT);
-            const db = client.db();
+      const message = {
+        message_type: req.body.message_type,
+        message_body: req.body.message_body,
+        user_id: req.body.user_id, // or req.session
+        date: date,
+      };
 
-            const messagesCollection = db.collection('messages');
-			const savedMessage = await messagesCollection.insertOne({message});
+      // add to db
+      const client = await MongoClient.connect(process.env.MONGO_CONNECT);
+      const db = client.db();
 
-			console.log(savedMessage);
+      const messagesCollection = db.collection("messages");
+      const savedMessage = await messagesCollection.insertOne({ message });
 
-			client.close();
+      console.log(savedMessage);
 
-			await res.send({ _id: savedMessage.insertedId, message });
-            
-        } catch (e) {
-            console.log('error ', e);
-			await res.status(403).json({error: 'unable to add message'});
-        }
-    } 
+      client.close();
+
+      await res.send({ _id: savedMessage.insertedId, message });
+    } catch (e) {
+      console.log("error ", e);
+      await res.status(403).json({ error: "unable to add message" });
+    }
+  }
 }
