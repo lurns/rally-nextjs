6add6a304c7e5ce7f55d544f3c68105d1f200822
Lauren Lee <lrnlee08@gmail.com>
2025-03-09 13:36:49 -0500

    login refactor

--- components/home/Home.js
+++ components/home/Home.js
@@ -1,48 +1,24 @@
-import LoginForm from '../../components/login/LoginForm';
-import { useRouter } from "next/router";
-import { useState, useContext } from 'react';
-import Image from 'next/image';
-import { WorkoutContext } from '../../store/workout-context';
+import LoginForm from "../../components/login/LoginForm";
+import Image from "next/image";
 
 const Home = () => {
-    // handle sending data to api/login
-    const router = useRouter();
-    const [error, setError] = useState(false);
-    const { getWorkouts } = useContext(WorkoutContext);
-
-    const loginHandler = async (userData) => {
-        const response = await fetch('/api/login', {
-        method: 'POST',
-        body: JSON.stringify(userData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-        });
-
-        const data = await response.json();
-
-        if (!data.error) {
-            router.push('/dash');
-            getWorkouts();
-        } else {
-            setError(true);
-        }
-
-    }
-    return (
-        <div className="flex h-full bg-purple-200 pb-5 pt-5">
-            <div className="md:flex sm:flex-wrap-reverse mx-auto bg-slate-600 self-center p-10 rounded-2xl shadow-lg">
-                <div className="flex-1">
-                    <Image alt="Motivate yourself!" width="500" height="500" src="https://res.cloudinary.com/dgnsgqoi9/image/upload/v1646167222/rally/Rally_copy_rx4xhm.png" />
-                </div>
-                <div className="flex-1">
-                    <LoginForm onLogin={loginHandler} error={error}/>
-                </div>
-            </div>
+  return (
+    <div className="flex md:h-screen bg-purple-200 pb-5 pt-5">
+      <div className="md:flex sm:flex-wrap-reverse mx-auto bg-slate-600 self-center p-10 rounded-2xl shadow-lg">
+        <div className="flex-1">
+          <Image
+            alt="Motivate yourself!"
+            width="500"
+            height="500"
+            src="https://res.cloudinary.com/dgnsgqoi9/image/upload/v1646167222/rally/Rally_copy_rx4xhm.png"
+          />
         </div>
-    )
-}
-
-
+        <div className="flex-1">
+          <LoginForm />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- components/login/LoginForm.js
+++ components/login/LoginForm.js
@@ -1,95 +1,123 @@
-import { useRef } from "react";
+import { useRef, useState, useContext } from "react";
+import { useRouter } from "next/router";
 import Link from "next/dist/client/link";
 import MessageBanner from "../ui/MessageBanner";
 import { ERROR_MESSAGE } from "../../constants/messageBannerType";
+import { WorkoutContext } from "../../store/workout-context";
 
-const LoginForm = (props) => {
-    const emailRef = useRef();
-    const passRef = useRef();
+const LoginForm = () => {
+  // handle sending data to api/login
+  const router = useRouter();
+  const { getWorkouts } = useContext(WorkoutContext);
+  const [updateRespsonse, setUpdateResponse] = useState({});
 
-    const submitHandler = (event) => {
-        event.preventDefault();
+  const emailRef = useRef();
+  const passRef = useRef();
 
-        const userData = {
-            email: emailRef.current.value,
-            password: passRef.current.value,
-        }
+  const loginHandler = async (event) => {
+    event.preventDefault();
 
-        props.onLogin(userData)
+    const userData = {
+      email: emailRef.current.value,
+      password: passRef.current.value,
+    };
+
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await response.json();
+
+      if (!data.error) {
+        router.push("/dash");
+        getWorkouts();
+      } else {
+        setUpdateResponse({ type: ERROR_MESSAGE, message: data.error });
+      }
+    } catch (e) {
+      setUpdateResponse({ type: ERROR_MESSAGE, message: "Unable to login." });
     }
+  };
 
-    return (
-        <>
-            <h2 className="text-left font-black text-6xl text-yellow-200">
-                Let&apos;s log those workouts!
-            </h2>
-            <h3 className="mt-3 text-left font-medium italic text-3xl text-slate-100">
-                Or else.
-            </h3>
-            {props.error ? <MessageBanner type={ERROR_MESSAGE} message="User not found. Sign up?" /> : ''}
-            <form id="rallyLogin" className="mt-10" onSubmit={submitHandler}>
-                <div className="flex flex-col mb-3">
-                <label 
-                        htmlFor="email"
-                        className="text-left text-white pb-1"
-                    >Email
-                    </label>
-                    <input 
-                        type="text"
-                        name="rallyEmail"
-                        id="rallyEmail"
-                        className="border-1 border-slate-300 bg-white rounded-lg p-1"
-                        ref={emailRef}
-                        required
-                    />
-                </div>
-                <div className="flex flex-col mb-3">
-                    <label 
-                        htmlFor="password"
-                        className="text-left text-white pb-1"
-                    >Password
-                    </label>
-                    <input
-                        type="password"
-                        name="rallyPass"
-                        id="rallyPass"
-                        className="border-1 border-slate-300 bg-white rounded-lg p-1"
-                        ref={passRef}
-                        required
-                    />
-                </div>
-                <div className="flex flex-col mb-3">
-                    <button className="
-                        transition 
-                        duration-200 
-                        ease-in-out 
-                        min-w-full 
-                        mt-5 
-                        p-4 
-                        bg-white 
-                        font-bold 
-                        text-purple-700 
-                        rounded-2xl 
-                        shadow-lg
-                        hover:bg-purple-400
-                        hover:text-white
-                        hover:cursor-pointer
-                    ">Log in</button>
-                </div>
-                <div>
-                    <p className="italic text-sm text-yellow-200">
-                        Need an account?&nbsp;
-                        <Link href="/signup">
-                            <span className="underline cursor-pointer">
-                                Sign up.        
-                            </span>
-                        </Link>
-                    </p>
-                </div>
-
-            </form>
-        </>
-    )
-}
+  return (
+    <>
+      <h2 className="text-left font-black text-6xl text-yellow-200">
+        Let&apos;s log those workouts!
+      </h2>
+      <h3 className="mt-3 text-left font-medium italic text-3xl text-slate-100">
+        Or else.
+      </h3>
+      {Object.keys(updateRespsonse).length > 0 && (
+        <MessageBanner
+          type={updateRespsonse.type}
+          message={updateRespsonse.message}
+        />
+      )}
+      <form id="rallyLogin" className="mt-10" onSubmit={loginHandler}>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="email" className="text-left text-white pb-1">
+            Email
+          </label>
+          <input
+            type="text"
+            name="rallyEmail"
+            id="rallyEmail"
+            className="border-1 border-slate-300 bg-white rounded-lg p-1"
+            ref={emailRef}
+            required
+          />
+        </div>
+        <div className="flex flex-col mb-3">
+          <label htmlFor="password" className="text-left text-white pb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            name="rallyPass"
+            id="rallyPass"
+            className="border-1 border-slate-300 bg-white rounded-lg p-1"
+            ref={passRef}
+            required
+          />
+        </div>
+        <div className="flex flex-col mb-3">
+          <button
+            className="
+              transition 
+              duration-200 
+              ease-in-out 
+              min-w-full 
+              mt-5 
+              p-4 
+              bg-white 
+              font-bold 
+              text-purple-700 
+              rounded-2xl 
+              shadow-lg
+              hover:bg-purple-400
+              hover:text-white
+              hover:cursor-pointer
+            "
+          >
+            Log in
+          </button>
+        </div>
+        <div>
+          <p className="italic text-sm text-yellow-200">
+            Need an account?&nbsp;
+            <Link href="/signup">
+              <span className="underline cursor-pointer">Sign up.</span>
+            </Link>
+          </p>
+        </div>
+      </form>
+    </>
+  );
+};
 
 export default LoginForm;
